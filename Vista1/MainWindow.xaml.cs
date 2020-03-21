5272ba3b215fa2ee6662c0b6dca561b939785d98
Daniela <da.osorios@alumnos.duoc.cl>
2020-03-21 12:17:58 -0300

    Boton eliminar, comentarios

--- Vista1/MainWindow.xaml.cs
+++ Vista1/MainWindow.xaml.cs
@@ -32,6 +32,8 @@ namespace Vista1
             Limpiar();
         }
 
+
+        //boton agregar a clientes 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
 
@@ -54,25 +56,26 @@ namespace Vista1
                     per.sexo = "Maculino";
                 }
 
-                Console.WriteLine("dfxdfxdf");
-                per.estadoCiv=cbsituacion.Text;
 
-                
+                per.estadoCiv = cbsituacion.Text;
+
+
 
                 misclientes.Add(per);
 
                 MostrarPersonas();
                 Limpiar();
             }
-            catch (Exception )
+            catch (Exception)
             {
-              
-                
+
+
             }
- 
+
 
         }
 
+        //metodo muestra clientes en la datagrid
         private void MostrarPersonas()
         {
             //las personas que se agregan a la lista se muestran aqui en el dategrid
@@ -80,6 +83,7 @@ namespace Vista1
             dgCliente.Items.Refresh();
         }
 
+           //metodo limpiar controles
         private void Limpiar()
         {
             txtRut.Text = string.Empty;
@@ -87,14 +91,15 @@ namespace Vista1
             txtApellidos.Text = string.Empty;
             dpFechaNac.SelectedDate = DateTime.Today;
             rbfemenino.IsChecked = false;
-            cbsituacion.SelectedIndex =0 ;
+            cbsituacion.SelectedIndex = 0;
         }
 
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-            Limpiar();
-        }
+        //private void Button_Click_1(object sender, RoutedEventArgs e)
+        //{
+        //    Limpiar();
+        //}
 
+        //boton buscar
         private void Button_Click_2(object sender, RoutedEventArgs e)
         {
             //busca en la coleccion al primer cliente que tenga el mismo rut
@@ -116,9 +121,6 @@ namespace Vista1
                     {
                         rbmasculino.IsChecked = true;
                     }
-                        
-                     
-
 
 
                 }
@@ -127,12 +129,69 @@ namespace Vista1
 
                     MessageBox.Show("Cliente no encontrado !");
                 }
-               
+
             }
             else
             {
                 MessageBox.Show("Debe ingresar el rut del cliente");
             }
         }
-    }  
+
+        //boton editar
+        private void bEditar_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Persona per = misclientes.First(p => p.rut == txtRut.Text);
+                per.nombres = txtNombre.Text;
+                per.apellidos = txtApellidos.Text;
+                per.fechaNaci = (DateTime)dpFechaNac.SelectedDate;
+                //sexo
+                Console.WriteLine();
+                if (rbfemenino.IsChecked.Equals(true))
+                {
+                    per.sexo = "Mujer";
+                }
+                else if (rbmasculino.IsChecked.Equals(true))
+                {
+                    per.sexo = "Maculino";
+                }
+                per.estadoCiv = cbsituacion.Text;
+                MessageBox.Show("Cliente Actualizado");
+
+                //para que el datagrid se actualice con los datos editados
+                MostrarPersonas();
+                Limpiar();
+
+            }
+            catch
+            {
+                MessageBox.Show("Cliente no encontrado!");
+            }
+        }
+
+        //boton eliminar
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                //mensaje que tiene boton si o no para confirmar eliminacion 
+               MessageBoxResult eliminar= MessageBox.Show("¿Está Seguro de eliminar este cliente?", "Confirmar",MessageBoxButton.YesNo,MessageBoxImage.Question);
+                Persona per = misclientes.First(p => p.rut == txtRut.Text);
+                misclientes.Remove(per);
+
+                if(eliminar== MessageBoxResult.Yes)
+                {
+                    misclientes.Remove(per);
+                    MostrarPersonas();
+                    Limpiar();
+                }
+                
+            }
+            catch
+            {
+                MessageBox.Show("Cliente no encontrado!");
+            }
+        }
+    }
 }
